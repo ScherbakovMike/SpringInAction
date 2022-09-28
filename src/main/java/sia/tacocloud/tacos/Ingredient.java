31156183fe546344f29d2772e931a79a5cb6b8f3
Mikhail Shcherbakov <scherbakov.mike@gmail.com>
2022-09-28 21:01:43 +0300

    init

--- src/main/java/sia/tacocloud/tacos/Ingredient.java
+++ src/main/java/sia/tacocloud/tacos/Ingredient.java
@@ -1,6 +1,7 @@
 package sia.tacocloud.tacos;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -10,13 +11,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document(collection = "ingredient")
 @RequiredArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor
+//@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
 public class Ingredient {
 
   @Id
-  private final String id;
-  private final String name;
-  private final Type type;
+  private String id;
+  private String name;
+  private Type type;
 
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
